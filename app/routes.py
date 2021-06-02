d54f8a8b9d18cb85237c277ae595b2a7bbef305a
Chris Chen <xinghanchen2019@gmail.com>
2021-06-01 22:17:44 -0600

    Chapter5-1

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,9 @@
+from werkzeug.security import check_password_hash
+from app.models import User
 from app import app
 from flask import render_template, flash, redirect, url_for
 from app.form import LoginForm
+from flask_login import current_user, login_user
 
 
 @app.route('/index')
@@ -21,9 +24,14 @@ def index():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
-        flash('Login request from user {}, remember_me {}'.format(
-            form.username.data, form.remember_me.data))
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash("Invalid username or password")
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
         return redirect(url_for('index'))
     return render_template('login.html', title='Sign In', form=form)
