e07e53457ab9f325a0822d4591b26469e495b7e6
Chris Chen <xinghanchen2019@gmail.com>
2021-06-03 22:25:56 -0600

    Chapter6-1

--- app/models.py
+++ app/models.py
@@ -1,6 +1,7 @@
 from app import db, login
 from datetime import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
+from hashlib import md5
 from flask_login import UserMixin
 
 class User(UserMixin, db.Model):
@@ -18,6 +19,10 @@ class User(UserMixin, db.Model):
     
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
+
+    def avatar(self, size):
+        digest = md5(self.email.lower().encode('utf-8')).hexdigest()
+        return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(digest,size)
 
 @login.user_loader
 def load_user(id):
