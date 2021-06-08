d677bbd124cbda8eaa212ec221c86bd9f046382d
Chris Chen <xinghanchen2019@gmail.com>
2021-06-07 20:13:59 -0600

    Chapter6-2

--- app/models.py
+++ app/models.py
@@ -10,6 +10,8 @@ class User(UserMixin, db.Model):
     email = db.Column(db.String(120), index=True,unique=True)
     password_hash = db.Column(db.String(128))
     posts = db.relationship('Post', backref='author', lazy='dynamic')
+    about_me = db.Column(db.String(140))
+    last_seen = db.Column(db.DateTime, default=datetime.utcnow)
 
     def __repr__(self) -> str:
         return '<User {}>'.format(self.username)
@@ -37,3 +39,4 @@ class Post(db.Model):
 
     def __repr__(self) -> str:
         return '<Post {}>'.format(self.body)
+
--- app/routes.py
+++ app/routes.py
@@ -1,12 +1,19 @@
+from datetime import datetime
 from flask_login.utils import login_required, logout_user
 from werkzeug.security import check_password_hash
 from app.models import User
 from app import app, db
 from flask import render_template, flash, redirect, url_for, request
-from app.form import LoginForm, RegistrationForm
+from app.form import EditProfileForm, LoginForm, RegistrationForm
 from flask_login import current_user, login_user, login_required
 from werkzeug.urls import url_parse
 
+
+@app.before_request
+def berfore_request():
+    if current_user.is_authenticated:
+        current_user.last_seen = datetime.utcnow()
+        db.session.commit()
 @app.route('/')
 @app.route('/index')
 @login_required
@@ -68,5 +75,58 @@ def user(username):
         {'author':user, 'body': 'Test post #1'},
         {'author':user, 'body': 'Test post #2'}
     ]
+    return render_template('user.html', user=user, posts=posts)
+
+
+@app.route('/edit_profile', methods=['GET', 'POST'])
+@login_required
+def edit_profile():
+    form = EditProfileForm()
+    if form.validate_on_submit():
+        current_user.username = form.username.data
+        current_user.about_me = form.about_me.data
+        db.session.commit()
+        flash("You changes have been saved!")
+        return redirect(url_for('edit_profile'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.about_me.data = current_user.about_me
+    return render_template('edit_profile.html', title='Edit Profile', form=form)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     return render_template('user.html', user=user, posts=posts)
