a66cb723cbe0d8b79d225264fa8cfb88a821baf8
Chris Chen <chris.xinghan.chen@gmail.com>
2021-06-09 22:14:43 -0600

    Chapter7-2

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
 import logging
-from logging.handlers import SMTPHandler
+from logging.handlers import RotatingFileHandler, SMTPHandler
+import os
 from flask import Flask
 from app.config import Config
 from flask_sqlalchemy import SQLAlchemy
@@ -28,7 +29,7 @@ if not app.debug:
             credentials=auth, secure=secure)
         mail_handler.setLevel(logging.ERROR)
         app.logger.addHandler(mail_handler)
-"""
+
     if not os.path.exists('logs'):
         os.mkdir('logs')
     file_handler = RotatingFileHandler('logs/microblog.log', maxBytes=10240,
@@ -40,5 +41,5 @@ if not app.debug:
 
     app.logger.setLevel(logging.INFO)
     app.logger.info('Microblog startup')
-"""
+
 from app import routes, models, errors
