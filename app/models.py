129b870c29381528b88e3761d320b742ccb5df1c
Chris Chen <chris.xinghan.chen@gmail.com>
2021-06-10 22:21:34 -0600

    Chapter7-1: followers database

--- app/models.py
+++ app/models.py
@@ -4,6 +4,11 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from hashlib import md5
 from flask_login import UserMixin
 
+followers = db.Table(
+    'followers',
+    db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
+    db.Column('followed_id', db.Integer, db.ForeignKey('user.id'))
+)
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
@@ -12,6 +17,12 @@ class User(UserMixin, db.Model):
     posts = db.relationship('Post', backref='author', lazy='dynamic')
     about_me = db.Column(db.String(140))
     last_seen = db.Column(db.DateTime, default=datetime.utcnow)
+    followed = db.relationship(
+        'User', secondary=followers,
+        primaryjoin=(followers.c.follower_id == id),
+        secondaryjoin=(followers.c.followed_id == id), 
+        backref=db.backref('folllowers', lazy='dynamic'), lazy='dynamic'
+    )
 
     def __repr__(self) -> str:
         return '<User {}>'.format(self.username)
