0ad73fbd16a7d9c829c62d620496d404a49d424d
Chris Chen <chris.xinghan.chen@gmail.com>
2021-06-13 12:50:02 -0600

    Chapter8-3

--- microblog/app/models.py
+++ microblog/app/models.py
@@ -21,7 +21,7 @@ class User(UserMixin, db.Model):
         'User', secondary=followers,
         primaryjoin=(followers.c.follower_id == id),
         secondaryjoin=(followers.c.followed_id == id), 
-        backref=db.backref('folllowers', lazy='dynamic'), lazy='dynamic'
+        backref=db.backref('followers', lazy='dynamic'), lazy='dynamic'
     )
 
     def __repr__(self) -> str:
@@ -48,7 +48,12 @@ class User(UserMixin, db.Model):
     def is_following(self, user):
         return self.followed.filter(
             followers.c.followed_id == user.id).count() > 0
-        
+    
+    def followed_posts(self):
+        followed = Post.query.join(
+            followers, (followers.c.followed_id == Post.user_id)).filter(
+                followers.c.follower_id == self.id)
+        return followed.union(self.posts).order_by(Post.timestamp.desc())
 
 
 
