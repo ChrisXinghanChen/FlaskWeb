e51a852e48d7c7328b2ceb5999c357a7ac752fec
Chris Chen <chris.xinghan.chen@gmail.com>
2021-06-14 22:23:06 -0600

    Chapter9: Explore

--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -15,8 +15,8 @@ def berfore_request():
         current_user.last_seen = datetime.utcnow()
         db.session.commit()
 
-@app.route('/')
-@app.route('/index')
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/index', methods=['GET', 'POST'])
 @login_required
 def index():
     form = PostForm()
@@ -24,20 +24,16 @@ def index():
         post = Post(body=form.post.data, author=current_user)
         db.session.add(post)
         db.session.commit()
-        flash('YOur post is now live!')
+        flash('Your post is now live!')
         return redirect(url_for('index'))
-    posts = [
-        {
-            'author':{'userName': 'Chris'},
-            'body':'I love Tia!'
-        },
-        {
-            'author':{'userName': 'Tia'},
-            'body':'I love Chris!'
-        }
-    ]
-    return render_template('index.html', form=form, posts=posts)
+    posts = current_user.followed_posts().all()
+    return render_template('index.html', title='Home Page', form=form, posts=posts)
 
+@app.route('/explore')
+@login_required
+def explore():
+    posts = Post.query.order_by(Post.timestamp.desc()).all()
+    return render_template('index.html', title='Explore', posts=posts)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
