334516c198b13f4e651ae83630118d7469ef20f0
Chris Chen <chris.xinghan.chen@gmail.com>
2021-06-16 18:45:58 -0600

    Chapter9: Pagination of blog posts

--- microblog/app/config.py
+++ microblog/app/config.py
@@ -12,3 +12,4 @@ class Config(object):
     MAIL_USERNAME = os.environ.get('MAIL_USRNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     ADMINS = ['xinghanchen2019@gmail.com']
+    POSTS_PER_PAGE = 3
