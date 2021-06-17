9260f777ce07e785307d4fbcf04d62358a99bf84
Chris Chen <chris.xinghan.chen@gmail.com>
2021-06-16 21:50:54 -0600

    Chapter9: Pagination in the user profile page

--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -29,8 +29,12 @@ def index():
     page = request.args.get('page', 1, type=int)
     posts = current_user.followed_posts().paginate(
         page, app.config['POSTS_PER_PAGE'], False)
+    next_url = url_for('index', page=posts.next_num) \
+        if posts.has_next else None
+    prev_url = url_for('index', page=posts.prev_num) \
+        if posts.has_prev else None
     return render_template('index.html', title='Home Page',
-    form=form, posts=posts.items)
+    form=form, posts=posts.items, next_url=next_url, prev_url=prev_url)
 
 @app.route('/explore')
 @login_required
@@ -38,8 +42,12 @@ def explore():
     page = request.args.get('page', 1, type=int)
     posts = Post.query.order_by(Post.timestamp.desc()).paginate(
         page, app.config['POSTS_PER_PAGE'], False)
+    next_url = url_for('index', page=posts.next_num) \
+    if posts.has_next else None
+    prev_url = url_for('index', page=posts.prev_num) \
+        if posts.has_prev else None
     return render_template('index.html', title='Explore',
-     posts=posts.items)
+     posts=posts.items, next_url=next_url, prev_url=prev_url)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -80,11 +88,15 @@ def register():
 @app.route('/user<username>')
 def user(username):
     user = User.query.filter_by(username=username).first_or_404()
-    posts = [
-        {'author':user, 'body': 'Test post #1'},
-        {'author':user, 'body': 'Test post #2'}
-    ]
-    return render_template('user.html', user=user, posts=posts)
+    page = request.args.get('page', 1, type=int)
+    posts = user.posts.order_by(Post.timestamp.desc()).paginate(
+        page, app.config['POSTS_PER_PAGE'], False)
+    next_url = url_for('user', username=username, page=posts.next_num) \
+    if posts.has_next else None
+    prev_url = url_for('user', username=username, page=posts.prev_num) \
+        if posts.has_prev else None
+    return render_template('user.html', user=user, posts=posts.items,
+                        next_url=next_url, prev_url=prev_url)
 
 
 @app.route('/edit_profile', methods=['GET', 'POST'])
